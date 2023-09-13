f8a274d2c1d8647c4537110859002ee0094773ff
cas-os <kazimierz.osinski@gmail.com>
2023-09-13 19:48:11 +0200

    first commit

---
+++ BasicTCPClient.py
@@ -0,0 +1,17 @@
+import socket
+
+print("Line :: 3")
+target_host = "www.google.com"
+target_port = 80
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect((target_host, target_port))
+
+
+request = "GET / HTTP/1.1\r\nHost: google.com\r\n\r\n"
+client.send(request.encode())
+
+response = client.recv(4096)
+print(response.decode())
+
+client.close()
